1273517773af558a1ef51ef4bd063aef5c1ad1be
Anton Lund <antonlund95@gmail.com>
2023-02-09 00:12:58 +0100

    chore: make AdminTournamentsPage publically available for demo purposes

--- src/webapp/src/pages/tournamentpage/AdminTournamentsPage.tsx
+++ src/webapp/src/pages/tournamentpage/AdminTournamentsPage.tsx
@@ -1,31 +1,15 @@
 import {CenteredPage} from "../../components/CenteredPage";
-import {Grid, makeStyles, Typography} from "@material-ui/core";
+import {Grid, Typography} from "@material-ui/core";
 import {Divider as CSDivider} from "../../components/Divider";
 import * as React from "react";
-import {useGetCurrentUser} from "../../hooks/api/useUser";
-import {UserRole} from "../../codegen/generated-types";
 import {useGetAllTournaments} from "../../hooks/api/useTournament";
 import {AdminTournamentsTable} from "./AdminTournamentsTable";
 
-const useStyles = makeStyles(theme => ({
-    headerText: {
-        borderLeft: "1px solid " + theme.palette.primary.main,
-        paddingLeft: theme.spacing(1)
-    }
-}))
-
 export const AdminTournamentsPage = (): JSX.Element => {
-    const {currentUser} = useGetCurrentUser();
     const {tournaments} = useGetAllTournaments();
 
     if (!tournaments) {
         return <></>
-    }
-
-    const sortedTournaments = tournaments.slice().sort((t1, t2) => Date.parse(t1.createdTs) - Date.parse(t2.createdTs))
-
-    if (currentUser?.role !== UserRole.Admin && currentUser?.role !== UserRole.Organizer) {
-        return <>You are not allowed to view this page</>
     }
 
     return <CenteredPage fullWidth>
