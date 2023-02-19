bbd2d04a9eb398d8f5c638804153efc7731592ee
Anton Lund <antonlund95@gmail.com>
2023-02-19 01:21:53 +0100

    chore: label stages in frontend docker file

--- image-frontend.dockerfile
+++ image-frontend.dockerfile
@@ -1,12 +1,12 @@
-FROM public.ecr.aws/nginx/nginx:1.19
-COPY src/webapp/nginx/nginx.conf etc/nginx/nginx.conf
-
-FROM public.ecr.aws/docker/library/node:lts-alpine3.17
+FROM public.ecr.aws/docker/library/node:lts-alpine3.17 AS build
 RUN mkdir -p /app
 WORKDIR /app
 COPY src/webapp/package*.json ./
 RUN npm install
 COPY src/webapp/ ./
 RUN npm run build
+
+FROM public.ecr.aws/nginx/nginx:1.19 AS base
+COPY src/webapp/nginx/nginx.conf etc/nginx/nginx.conf
 
 COPY --from=build /app/build/ /usr/share/nginx/html/
--- src/test/kotlin/com/antonl/cssundays/unit/model/tournaments/BracketTreeTest.kt
+++ src/test/kotlin/com/antonl/cssundays/unit/model/tournaments/BracketTreeTest.kt
@@ -3,29 +3,25 @@ package com.antonl.cssundays.unit.model.tournaments
 import com.antonl.cssundays.model.core.Team
 import com.antonl.cssundays.model.core.User
 import com.antonl.cssundays.model.tournaments.*
+import org.junit.Before
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 internal class BracketTreeTest {
 
+    @BeforeEach
+
     @Test
-    fun generateBracket() {
-        val teams = listOf<Team>(
-            Team(name = "Test", owner = User(playertag = "", email = "")),
-            Team(name = "Test", owner = User(playertag = "", email = "")),
-            Team(name = "Test", owner = User(playertag = "", email = "")),
-            Team(name = "Test", owner = User(playertag = "", email = "")),
-            Team(name = "Test", owner = User(playertag = "", email = "")),
-            Team(name = "Test", owner = User(playertag = "", email = "")),
-            Team(name = "Test", owner = User(playertag = "", email = "")),
-            Team(name = "Test", owner = User(playertag = "", email = "")),
-            Team(name = "Test", owner = User(playertag = "", email = "")),
-            Team(name = "Test", owner = User(playertag = "", email = "")),
-            Team(name = "Test", owner = User(playertag = "", email = ""))
-        )
+    fun generateBracket(numberOfTeams: Int) {
+        val teams = mutableListOf<Team>()
+
+        for (i in 0..numberOfTeams) {
+            Team(id = i, name = "Team${1}", owner = User(playertag = "Player${i}", email = "Player${i}@mail.com"))
+        }
 
         val bracket = Bracket()
-        BracketMatchInitializer(15).traverseTree(bracket)
+        val numberOfMatches = BracketCalculator.calculateNumberOfMatchesInBracket(numberOfTeams)
+        BracketMatchInitializer(numberOfMatches).traverseTree(bracket)
         BracketTeamPopulator(teams).populateTree(bracket)
-        BracketTreePrinter().traverseTree(bracket)
     }
 }
