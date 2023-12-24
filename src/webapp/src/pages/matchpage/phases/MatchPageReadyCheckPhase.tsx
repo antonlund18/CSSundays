10cd0272110725c12c681280f6a20995c216b311
Anton Lund <ALD@ramboll.com>
2023-12-24 17:22:53 +0100

    create styling for ready check phase

--- src/webapp/src/pages/matchpage/phases/MatchPageReadyCheckPhase.tsx
+++ src/webapp/src/pages/matchpage/phases/MatchPageReadyCheckPhase.tsx
@@ -1,13 +1,26 @@
 import * as React from "react"
-import {Box, Button, CircularProgress, Typography} from "@mui/material";
-import {PlayerPicture} from "../../teamspage/team/PlayerPicture";
-import {Test} from "../Test";
-import {activeDutyMapPool, CS2Map} from "../../../util/MapPool";
-import {useEffect, useState} from "react";
 import {makeStyles} from "@mui/styles";
 import {User} from "../../../codegen/generated-types";
+import {Box, CircularProgress, Grid, Typography} from "@mui/material";
+import {PlayerPicture} from "../../teamspage/team/PlayerPicture";
+import {Check} from "@mui/icons-material";
 
 const useStyles = makeStyles(theme => ({
+    captain: {
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100%"
+    },
+    captainSpinner: {
+        top: "50%",
+        left: "50%",
+        position: "absolute",
+        transform: "translate(-50%, -50%)",
+        zIndex: 100,
+    }
 }))
 
 type MatchPageReadyCheckPhaseProps = {
@@ -16,5 +29,30 @@ type MatchPageReadyCheckPhaseProps = {
 }
 
 export const MatchPageReadyCheckPhase = (props: MatchPageReadyCheckPhaseProps) => {
-    return <>aber</>
+    const classes = useStyles()
+
+
+    return <Grid container sx={{height: "100%", justifyContent: "center", alignItems: "center"}}>
+        <Grid item xs={4} sx={{display: "flex", justifyContent: "center"}}>
+            {props.team1Captain &&
+                <Box className={classes.captain}>
+                    <Box className={classes.captainSpinner}>
+                        <CircularProgress style={{zIndex: 100}}/>
+                    </Box>
+                    <PlayerPicture player={props.team1Captain} style={{width: "100px"}}/>
+                </Box>}
+        </Grid>
+        <Grid item xs={4} sx={{display: "flex", textAlign: "center"}}>
+            <Typography variant={"h2"}>Venter på hold bliver klar</Typography>
+        </Grid>
+        <Grid item xs={4} sx={{display: "flex", justifyContent: "center"}}>
+            {props.team2Captain &&
+                <Box className={classes.captain}>
+                    <Box className={classes.captainSpinner}>
+                        <Check style={{fontSize: "48px", color: "#39c900"}}/>
+                    </Box>
+                    <PlayerPicture player={props.team2Captain} style={{width: "100px"}}/>
+                </Box>}
+        </Grid>
+    </Grid>
 }
